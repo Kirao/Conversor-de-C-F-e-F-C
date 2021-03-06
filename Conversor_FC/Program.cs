5b856eed8419fdd16dddeaeef2b355c7047601d2
Guilherme Henrique Bodo <guihenrique1231@hotmail.com>
2021-03-06 15:48:07 -0300

    Conversor de °C / °F  e °F / °C

---
+++ Conversor_FC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Conversor_FC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int escolha;
+            double celsim, fairim, convCF, convFC;
+
+            Console.WriteLine("Oque deseja tranformar? [1] °C para °F ou [2] °F para °C");
+            escolha = int.Parse(Console.ReadLine());
+            
+
+            if (escolha == 1)
+            {
+                Console.Write("Insira o valor de °C para tranformar em °F: ");
+                celsim = double.Parse(Console.ReadLine());
+
+                convCF = (celsim * 9/5) + 32;
+
+                Console.WriteLine($"{celsim}°C é = {convCF.ToString("F2")}°F ");
+
+            }
+
+            else
+            {
+                Console.Write("Insira o valor de °F para tranformar em °C: ");
+                fairim = double.Parse(Console.ReadLine());
+
+                convFC = (fairim -32 ) * 5/9;
+
+                Console.WriteLine($"{fairim}°F é = {convFC.ToString("F2")}°C a ");
+            }
+
+            
+        }
+
+    }
+}
