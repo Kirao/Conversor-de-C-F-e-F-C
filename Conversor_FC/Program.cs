3c3fe0a2eb3f1d7b808ed0d4f4ec08e95ea15c30
Guilherme Henrique Bodo <guihenrique1231@hotmail.com>
2021-03-07 12:48:16 -0300

    Adicionando comentario a programação

--- Conversor_FC/Program.cs
+++ Conversor_FC/Program.cs
@@ -6,37 +6,43 @@ namespace Conversor_FC
     {
         static void Main(string[] args)
         {   
+            //Área de declaração das Variaveis
             int escolha;
             double celsim, fairim, convCF, convFC;
 
-            Console.WriteLine("Oque deseja tranformar? [1] °C para °F ou [2] °F para °C");
+            //Area de Escolha de conversão
+            Console.WriteLine("\nEscolha oque deseja tranformar? ");
+            Console.WriteLine("[1] °C para °F");
+            Console.WriteLine("[2] °F para °C");
+
             escolha = int.Parse(Console.ReadLine());
             
-
+             //Se a escolha dor 1 executa essa area de comando
             if (escolha == 1)
             {
-                Console.Write("Insira o valor de °C para tranformar em °F: ");
-                celsim = double.Parse(Console.ReadLine());
+                Console.Write("Insira o valor de °C para tranformar em °F: ");       // Msg para inserir qual Valor de Graus Celsius que deseja tranformar em Grau Fahrenheit
+                celsim = double.Parse(Console.ReadLine());                           // Vinculando o valor escolhido á variavel
 
-                convCF = (celsim * 9/5) + 32;
+                convCF = (celsim * 9/5) + 32;                                        //Formula de conversao de  °C  para  °F 
 
-                Console.WriteLine($"{celsim}°C é = {convCF.ToString("F2")}°F ");
+                Console.WriteLine($"{celsim}°C é = {convCF.ToString("F2")}°F ");     //Motra o valor escolhido em  °C e o resultado da conversao em  °F
 
             }
-
+            // Se o resultado for 2 executa essa area de comando
             else if (escolha == 2)
             {
-                Console.Write("Insira o valor de °F para tranformar em °C: ");
-                fairim = double.Parse(Console.ReadLine());
+                Console.Write("Insira o valor de °F para tranformar em °C: ");       // Msg para inserir qual Valor de Graus Fahrenheint que deseja tranformar em Grau Celsius
+                fairim = double.Parse(Console.ReadLine());                           // Vinculando o valor escolhido á variavel
 
-                convFC = (fairim -32 ) * 5/9;
+                convFC = (fairim -32 ) * 5/9;                                        //Formula de conversao de  °C  para  °F 
 
-                Console.WriteLine($"{fairim}°F é = {convFC.ToString("F2")}°C a ");
+                Console.WriteLine($"{fairim}°F é = {convFC.ToString("F2")}°C ");   //Motra o valor escolhido em  °F e o resultado da conversao em  °C
             }
-
+             
+            //Se a escolha nao for 1 ou 2 ele executa essa area de comando
             else
             {
-                Console.WriteLine("Nenhum dos modos escolhido!");
+                Console.WriteLine("Nenhum dos modos escolhido!");       //msg que indica que nao foi escolhido umas das opçoes disponiveis
             }
 
             
